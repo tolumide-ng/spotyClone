41e662e45a0212705e14884e429322044567371a
tolumide-ng <tolumideshopein@gmail.com>
2026-05-28 02:03:09 +0200

    Adds background widget and no LLM autocomplete settings in vscode

---
+++ lib/core/configs/assets/app_images.dart
@@ -0,0 +1,5 @@
+class AppImages {
+  static const String basePath = 'assets/images/';
+
+  static const String introBackground = '${basePath}intro_bg.png';
+}
--- lib/presentation/intro/page/get_started.dart
+++ lib/presentation/intro/page/get_started.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:spotify/core/configs/assets/app_images.dart';
+import 'package:spotify/core/configs/assets/app_vector.dart';
 
 class GetStartedPage extends StatelessWidget {
   const GetStartedPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(AppImages.introBackground),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(AppVector.spotifyLogo),
+                ),
+                Spacer(),
+                Text(
+                  'Enjoy listening to music',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                ElevatedButton(onPressed: () {}, child: Text('Get Started')),
+              ],
+            ),
+          ),
+
+          Container(color: Colors.black.withValues(alpha: 0.15)),
+        ],
+      ),
+    );
   }
 }
