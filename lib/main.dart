fe5841e6c6d26f3694e9c9c6afeb82ad25141fb1
tolumide-ng <tolumideshopein@gmail.com>
2026-05-30 01:16:11 +0200

    Sets up firebase on the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
-// import 'package:spotify/core/configs/theme/app_colors.dart';
 import 'package:spotify/core/configs/theme/app_theme.dart';
 import 'package:spotify/presentation/choose_mode/bloc/theme_cubit.dart';
 import 'package:spotify/presentation/splash/pages/splash.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,18 +16,12 @@ Future<void> main() async {
         ? HydratedStorageDirectory.web
         : HydratedStorageDirectory((await getTemporaryDirectory()).path),
   );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
-  // MaterialApp(
-  //     theme: AppTheme.lightTheme,
-  //     debugShowCheckedModeBanner: false,
-  //     home: const SplashPage(),
-  //   );
-
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
