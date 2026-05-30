73a53ad6284edb280cd2efcfa0e273674dca42fe
tolumide-ng <tolumideshopein@gmail.com>
2026-05-30 02:27:34 +0200

    test state

---
+++ lib/data/repository/auth/auth_repository_impl.dart
@@ -0,0 +1,15 @@
+import 'package:spotify/domain/repository/auth/auth.dart';
+
+class AuthRepositoryImpl extends AuthRepository {
+  @override
+  Future<void> signup() {
+    // TODO: implement signup
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> login() {
+    // TODO: implement login
+    throw UnimplementedError();
+  }
+}
---
+++ lib/data/sources/auth/auth_firebase_service.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:spotify/data/models/auth/create_user_req.dart';
+
+abstract class AuthFirebaseService {
+  Future<void> signup(CreateUserReq createUserReq);
+
+  Future<void> login();
+}
+
+class AuthFirebaseServiceImpl extends AuthFirebaseService {
+  @override
+  Future<void> signup(CreateUserReq createUserReq) async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: createUserReq.email,
+        password: createUserReq.password,
+      );
+    } on FirebaseAuthException catch (e) {}
+  }
+
+  @override
+  Future<void> login() {
+    // TODO: implement login
+    throw UnimplementedError();
+  }
+}
